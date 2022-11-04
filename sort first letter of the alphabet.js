17642be6501740257efbc3ffffaf2cd17a3ed9cf
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2022-11-04 15:12:07 +0200

    Add files via upload

---
+++ sort first letter of the alphabet.js
@@ -0,0 +1,16 @@
+const array = ['apple', 'programming', 'illya', 'dima', 'roman', 'java'];
+
+function sort(array)       
+{                              
+    for (let i = 0; i < array.length; i++){
+      let result = array[i];
+      let j = i - 1;
+       while (j >= 0 && array[j] > result){
+         array[j+1] = array[j]; j--;
+       }
+       array[j+1] = result;
+     }                    
+    console.log(array);
+}
+
+sort(array)
